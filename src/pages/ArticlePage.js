c61957a89d50e8c17e53f0ef0cd19868729e1cb7
siturbe <37549582+siturbe@users.noreply.github.com>
2020-01-07 20:42:51 -0600

    Item Modal & User (#18)
    * reverting code
    
    * Added authentication
    
    * Trying to get header.js to save as Header.js
    
    * Deleting file
    
    * Adding image capture, adding adding item, and restructuring backend
    
    * Adding modal that allows for inputing new item, but not yet fully functional.  I also changed the server and simpliefied MongoDB so I could check if it was working logically
    
    * Adding functionality so that username is consistent across whole session until logout

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import HomeBtn from '../components/HomeBtn';
+import ImageInput from '../components/ImageInput';
 import ArticleCarouselItem from '../components/ArticleCarouselItem';
-import articlesInfo from '../articles.json'
+import articlesInfo from '../articles.json';
+import Modal from '../components/Modal';
+import Header from '../components/Header';
 
 class ArticlePage extends React.Component {
   constructor(props) {
@@ -11,19 +14,35 @@ class ArticlePage extends React.Component {
     }
   }
 
+  state = {
+    show: false
+    };
+
+    showModal = e => {
+      this.setState({
+        show: !this.state.show   //toggles the show state
+      });
+
+
+    };
   
+
   render() {
  
-  
     const styles = {
       // config these style using sass
       carouselStyle: {
         height: "500px",
         display: "flex"
+      },
+      homeBtnStyle: {
+        width: "200px"
       }
     }
     return (
+      
       <div>
+        <Header title="Article Page" />
         <h1>
           ArticlePage
         </h1>
@@ -31,37 +50,51 @@ class ArticlePage extends React.Component {
           <span><HomeBtn title="Filter Tops" className="btn" link="#" /></span>
           <span><HomeBtn title="Filter Bottoms" className="btn" link="#" /></span>
         <div  className="container">
-        <div className="carousel" style={styles.carouselStyle}>
-          {/*filter articles from dB */}
-          {/*map articles and pass props from dB */}
-          {/*materialbox image to zoom on article */}
-          {Object.values(articlesInfo).map( article => {
-            const keywords = article.keywords
+          <div className="carousel" style={styles.carouselStyle}>
+            {/*filter articles from dB */}
+            {/*map articles and pass props from dB */}
+            {/*materialbox image to zoom on article */}
+            {Object.values(articlesInfo).map( article => {
+              const keywords = article.keywords
+              
+              
+      
+              return (<ArticleCarouselItem 
+                name={article.name}
+                key={article.id}
+                id={article.id}
+                type={article.type}
+                image={article.image}
+                keywords={article.keywords}
+                goesWith={article.goesWith}
+              />)
+              })}
+              
             
             
-    
-            return (<ArticleCarouselItem 
-              name={article.name}
-              key={article.id}
-              id={article.id}
-              type={article.type}
-              image={article.image}
-              keywords={article.keywords}
-              goesWith={article.goesWith}
-            />)
-            })}
             
-          
-          
-          
-        </div>
+          </div>
+
+          <div>
+          <Modal onClose={this.showModal} show={this.state.show}>
+            <ImageInput/>
+          </Modal>
+          </div>
 
         </div>
        
 
 
         <span><HomeBtn title="Home" className="btn" link="./" /></span>
-        <span><HomeBtn title="New" className="btn" link="#" /></span>
+        <span><button 
+          title="New" 
+            className="btn"
+            style={styles.homeBtnStyle}
+            onClick={ e => {
+              this.setState({
+                show: true
+              });
+            }}>ADD ITEM</button></span>
         <span><HomeBtn title="Burn Everything" className="btn" link="#" /></span>
 
       </div>
