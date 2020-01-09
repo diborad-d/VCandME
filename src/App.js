c81509761803db55cf994743b8ab81d1f17eb45f
Dibora D <diborad@yahoo.com>
2020-01-09 12:10:29 -0600

    changes from previos pull

--- src/App.js
+++ src/App.js
@@ -1,79 +1,67 @@
-import React, {useContext} from 'react';
-import 'bulma/css/bulma.css';
-import { useAuth0 } from './contexts/auth0-context';
-import Header from './components/Header';
+import React, { useContext } from "react";
+import "bulma/css/bulma.css";
+import { useAuth0 } from "./contexts/auth0-context";
+import Header from "./components/Header";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ClosetPage from "./pages/ClosetPage";
 import OutfitPage from "./pages/OutfitPage";
 import ArticlePage from "./pages/ArticlePage";
 
-
-
-import './App.css';
-import HomeBtn from './components/HomeBtn';
+import "./App.css";
+import HomeBtn from "./components/HomeBtn";
 
 // import user here
 
 function App() {
   const { isLoading, user, loginWithRedirect, logout } = useAuth0();
 
-  return(
-
+  return (
     <Router>
       <div>
         <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/closetPage" component={ClosetPage} />
+          <Route exact path="/outfitPage" component={OutfitPage} />
+          <Route exact path="/articlePage" component={ArticlePage} />
 
+          <Route exact path="/auth">
+            <>
+              <Header />
+              <div className="hero is-info is-fullheight">
+                <div className="hero-body">
+                  <div className="container has-text-centered">
+                    {!isLoading && !user && (
+                      <>
+                        <h1>Click Below!</h1>
+                        <button onClick={loginWithRedirect} className="button is-danger">
+                          Login
+                        </button>
+                      </>
+                    )}
+                    {!isLoading && user && (
+                      <>
+                        <h1>You are logged in!</h1>
+                        <p>Hello {user.name}</p>
 
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/closetPage" component={ClosetPage} />
-        <Route exact path="/outfitPage" component={OutfitPage} />
-        <Route exact path="/articlePage" component={ArticlePage} />
-       
-        <Route exact path="/auth">
-          <>
-          <Header />
-          <div className="hero is-info is-fullheight">
-            <div className="hero-body">
-              <div className="container has-text-centered">
-                {!isLoading && !user && (
-                  <>
-                    <h1>Click Below!</h1>
-                    <button onClick={loginWithRedirect} className="button is-danger">
-                      Login
-                    </button>
-                  </>
-                )}
-                {!isLoading && user && (
-                  <>
-                    <h1>You are logged in!</h1>
-                    <p>Hello {user.name}</p>
+                        {user.picture && <img src={user.picture} alt="My Avatar" />}
+                        <hr />
 
-                    {user.picture && <img src={user.picture} alt="My Avatar" />}
-                    <hr />
-
-                    <button
-                      onClick={() => logout({ returnTo: window.location.origin })}
-                      className="button is-small is-dark"
-                    >
-                      Logout
-                </button>
-                  </>
-                )}
+                        <button onClick={() => logout({ returnTo: window.location.origin })} className="button is-small is-dark">
+                          Logout
+                        </button>
+                      </>
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          </>
-        </Route>
-      </Switch>
+            </>
+          </Route>
+        </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
