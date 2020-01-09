75e3fb3c2a9fb1ee87fec3fc18bbc03fa4ba1c27
Dibora D <diborad@yahoo.com>
2020-01-09 12:09:37 -0600

    added header to pages, carousel in closet page and cards for article

--- src/pages/ClosetPage.js
+++ src/pages/ClosetPage.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button, IconButton, Grid, Paper, Tab, Tabs } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import Header from "../components/Header";
+import ArticleCarouselItem from "../components/ArticleCarouselItem";
+import ClosetSwipeableTextMobileStepper from "../components/ClosetCarousel";
+import closetItemCard from "../components/closetItemCard";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,19 +22,47 @@ const useStyles = makeStyles(theme => ({
 
 export default function ClosetPage() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(2);
+  const style = {
+    paper: {
+      padding: 5,
+      margin: 25,
+      width: "100%",
+      backgroundColor: "lightGray",
+      height: "100vh",
+      textAlign: "center"
+    }
+  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h4" className={classes.title}>
-          Vitual Closet
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  </div>
+    <div>
+      <Header />
+      <Grid container >
+        <Grid leftGrid sm spacing={3} style={style.paper}>
+          <Paper square>
+            <Tabs value={value} indicatorColor="primary" textColor="primary" onChange={handleChange} aria-label="disabled tabs example">
+              <Tab label="Article Deatials" />
+              <Tab label="Last Worn" />
+              <Tab label="Goes With" />
+            </Tabs>
+          </Paper>
+          <closetItemCard/>
+        </Grid>
+        <Grid topRightGrid sm style={style.paper}>
+          <Tabs>
+            <ClosetSwipeableTextMobileStepper />
+          </Tabs>
+          <Divider/>
+          <Grid bottomRightGrid sm>
+            <Tabs>
+              <ClosetSwipeableTextMobileStepper />
+            </Tabs>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
