49b6a791731c6864a2cc3b49ea008ff40a4fd9ec
Dibora D <diborad@yahoo.com>
2020-01-09 19:48:48 -0600

    added log in auth0 into header

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { useAuth0 } from '../contexts/auth0-context';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +21,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Header() {
+  const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -31,7 +34,27 @@ export default function Header() {
         <Typography variant="h4" className={classes.title}>
           Virtual Closet
         </Typography>
-        <Button color="inherit">Login</Button>
+        {/* <Button color="inherit">Login</Button> */}
+        <div className="navbar-end">
+              {/* if there is no user. show the login Button */}
+              {!isLoading && !user && (
+                <Button onClick={loginWithRedirect} className="navbar-item">
+                  Login
+                </Button>
+              )}
+              {/* if there is a user. show user name and logout Button */}
+              {!isLoading && user && (
+                <>
+                  <Button className="navbar-item">{user.name}</Button>
+                  <Button
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                    className="navbar-item"
+                  >
+                    Logout
+                  </Button>
+                </>
+              )}
+            </div>
       </Toolbar>
     </AppBar>
   </div>
