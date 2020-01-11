86d82c68834ff50d0d6707056a6c249c8ff0dc03
Dibora D <diborad@yahoo.com>
2020-01-10 19:50:24 -0600

    upgraded babel

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { useAuth0 } from '../contexts/auth0-context';
 
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
