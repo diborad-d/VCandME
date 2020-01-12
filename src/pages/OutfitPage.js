db8e2e335a1287f8657bfb50d8a068590a66f9f3
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-12 11:18:20 -0600

    Added Viewer to outfit  page

--- src/pages/OutfitPage.js
+++ src/pages/OutfitPage.js
@@ -1,60 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 import HomeBtn from "../components/HomeBtn";
 import EventListItem from "../components/EventListItem";
-import { outfit1 } from "../outfits.json";
+import OutfitViewer from "../components/OutfitViewer.component";
+import OutfitDetails from "../components/OutfitDetails.component";
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import outfits  from "../outfits.json";
 
 
-class ArticlePage extends React.Component {
+
+// maybe fs to read files
+
+
+
+export default  class OutfitPage extends Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+
+    this.handleOutfitChange = this.handleOutfitChange.bind(this);
+
+    this.state = {
+      currentOutfitIndex: 0,
+    }
+  }
+
+  componentDidMount(){
+    console.log("Axios outfit/articles and push to oufit array")
+  }
+
+
+  handleOutfitChange(outfitIndex) {
+    console.log(outfitIndex)
+    console.log("changed")
+    //pull from data base and change state.
+
+    this.setState({
+      currentOutfitIndex: outfitIndex,
+    })
+
+    //axios with index
+    //Top_Id Bottom_Id
+    //set set state
   }
 
   render() {
+    let index = this.state.currentOutfitIndex
     return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="col s8">
-              <h1>Current Outfit</h1>
-              <div className="row">
-                <div className="col s12">
-                  <h3>Top</h3>
-                  <img class="materialboxed" width="auto" src="./images/articles/article1.jpg"></img>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col s12">
-                  <h3>Bottom</h3>
-                  <img class="materialboxed" width="auto" src="./images/articles/article4.jpg"></img>
-                </div>
-              </div>
-            </div>
-            <div className="col s4">
-              <div className="row">
-                <div className="col s12">
-                  <h2>Photo collections</h2>
-                  <img class="materialboxed" width="auto" src="./images/mensClothing.jpg"></img>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col s12">
-                  <h2>Event collections</h2> {/*may be a modal for the events to show notable attendeess on collection link click*/}
-                  <div className="collection">
-                    {" "}
-                    {/**refactor collection into EventCollection */}
-                    {Object.values(outfit1.events).map(event => {
-                      return <EventListItem name={event.name} location={event.location} date={event.date} />;
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg">           
+          <div> 
+            <Grid container spacing={1}>
+              <Grid item xs>
+             <OutfitViewer 
+              outfits={outfits}
+              onChange={this.handleOutfitChange}/>
+              </Grid>
 
-export default ArticlePage;
+              <Grid item xs container direction="column" alignItems="flex-end" justify="flex-start">
+                <Grid item>
+                  <OutfitDetails 
+                    currentOutfit={outfits[index]["name"]} //data from state
+                    events={outfits[index]["events"]}
+                    attendees={outfits[index]["attendees"]}
+                    dates={outfits[index]["dates"]}>
+                  </OutfitDetails>
+                </Grid>
+                
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </React.Fragment>
+    )
+  }
+
+  
+
+  
+
+ 
+}
+  
