8eb2eaea6c8f4e125bf44fce0b590fb6da8df647
siturbe <37549582+siturbe@users.noreply.github.com>
2020-01-17 15:02:01 -0600

    Going Pruple (#32)
    * reverting code
    
    * Added authentication
    
    * Trying to get header.js to save as Header.js
    
    * Deleting file
    
    * Adding image capture, adding adding item, and restructuring backend
    
    * Adding modal that allows for inputing new item, but not yet fully functional.  I also changed the server and simpliefied MongoDB so I could check if it was working logically
    
    * Adding functionality so that username is consistent across whole session until logout
    
    * Adding pullsed changes
    
    * Changing the folder structure, adding ability to upload pictures, and have dtabase working on backend
    
    * Changed the input variable
    
    * Commiting the incomeing changes
    
    * Added ability to populate user pictures
    
    * Adding functionality of database to images, but mostly just on Top Carousel
    
    * updated file with simpler ui
    
    * Added changes to get most up to date code working

---
+++ server.js
@@ -0,0 +1,69 @@
+let express = require('express'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    dbConfig = require('./database/db');
+    path = require('path');
+
+const api = require('./routes/item.routes')
+const app = express();
+const port = process.env.PORT || 4000;
+const routes = require('./routes');
+
+// MongoDB Configuration
+// mongoose.Promise = global.Promise;
+// mongoose.connect(dbConfig.db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Database sucessfully connected')
+// },
+//     error => {
+//         console.log('Database could not be connected: ' + error)
+//     }
+// )
+
+
+//Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+//Define middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+
+//Add routes
+app.use('/public', express.static('public'));
+app.use('/api', api);
+app.use('/', routes);
+
+//Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/closetItems', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true
+     }).then(() => {console.log('Database successfully connected')})
+
+app.listen(port, () => {
+    console.log('Connected to port ' + port)
+})
+
+
+// app.use((req, res, next) => {
+//     // Error goes via `next()` method
+//     setImmediate(() => {
+//         next(new Error('Something went wrong'));
+//     });
+// });
+
+// app.use(function (err, req, res, next) {
+//     console.error(err.message);
+//     if (!err.statusCode) err.statusCode = 500;
+//     res.status(err.statusCode).send(err.message);
+// });
+
+
+//Changes to Server done after this
